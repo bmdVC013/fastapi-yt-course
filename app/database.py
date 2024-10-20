0df95a67145f5d39738166f67b901607c0bb2893
Dung Bui <bmdVC013@gmail.com>
2024-10-20 13:51:07 +0700

    feat: add Procfile

--- app/database.py
+++ app/database.py
@@ -1,11 +1,15 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .config import settings
+import psycopg2
 
 
 # postgresql://postgres:password@localhost:5432/fastapi
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.db_username}:{settings.db_password}@{settings.db_hostname}:{settings.db_port}/{settings.db_name}'
+# SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.db_username}:{settings.db_password}@{settings.db_hostname}:{settings.db_port}/{settings.db_name}'
+SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1)
+
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
