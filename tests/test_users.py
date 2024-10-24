80cdb0a03a0fc13c88a1b97adb9fe1b40882e261
Dung Bui <bmdVC013@gmail.com>
2024-10-24 23:46:25 +0700

    feat: add incorrect login tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -16,3 +16,14 @@ def test_login_user(test_user, client):
   assert id == test_user['id']
   assert login_res.token_type == "bearer"
   assert res.status_code == 200
+
+@pytest.mark.parametrize("email, password, status_code", [
+  ('wrongemail@gmail.com', 'password123', 403),
+  ('minhdung.bui13@gmail.com', 'wrongpassword', 403),
+  ('bmdvc013@gmail.com', 'wrongpassword', 403),
+  (None, 'password123', 403),
+  ('bmdvc013@gmail.com', None, 403)
+])
+def test_incorrect_login(test_user, client, email, password, status_code):
+  res = client.post("/login", data = {"username": email, "password": password})
+  assert res.status_code == status_code
